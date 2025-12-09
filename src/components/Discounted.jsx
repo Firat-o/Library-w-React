0617fe48be5e5b6b0d0c07c1463c46b2a38b7f5b
Firato <142419225+Firat-o@users.noreply.github.com>
2025-12-09 11:08:36 +0100

    Refactor Discounted component and update imports

--- src/components/Discounted.jsx
+++ src/components/Discounted.jsx
@@ -1,22 +1,22 @@
 import React from "react";
-import { books } from "../data.js";
-import Book from "./ui/Book.jsx";
+import { books } from "../data";
+import Book from "./ui/Book";
 
 const Discounted = () => {
   return (
-    <section className="" id="recent">
+    <section id="discounted">
       <div className="container">
         <div className="row">
           <h2 className="section__title">
-            Discounted <span className="blue">Books</span>
+            Discounted <span style={{ color: "#d4a373" }}>Books</span>
           </h2>
           <div className="books">
             {books
-            .filter(book => book.salePrice > 0 )
-            .slice(0, 8)
-            .map((book) => (
-              <Book book={book} key={book.id} />
-            ))}
+              .filter((book) => book.salePrice > 0)
+              .slice(0, 8)
+              .map((book) => (
+                <Book book={book} key={book.id} />
+              ))}
           </div>
         </div>
       </div>
