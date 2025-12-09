05e4adb110f80994d01624027ce0f15c07c93599
Firato <142419225+Firat-o@users.noreply.github.com>
2025-12-09 11:00:39 +0100

    Refactor Book component for image loading and display

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -5,8 +5,7 @@ import Price from "./Price";
 
 const Book = ({ book }) => {
   const [img, setImg] = useState();
-
-const mountedRef = useRef (true)
+  const mountedRef = useRef(true);
 
   useEffect(() => {
     const image = new Image();
@@ -14,17 +13,14 @@ const mountedRef = useRef (true)
     image.onload = () => {
       setTimeout(() => {
         if (mountedRef.current) {
-
           setImg(image);
         }
       }, 300);
     };
     return () => {
-
-      //When the component unmounts
       mountedRef.current = false;
-    }
-  });
+    };
+  }, [book.url]);
 
   return (
     <div className="book">
@@ -32,10 +28,10 @@ const mountedRef = useRef (true)
         <>
           <Link to={`/books/${book.id}`}>
             <figure className="book__img--wrapper">
-              <img src={img.src} alt="" className="book__img" />
+              <img src={img.src} alt={book.title} className="book__img" />
             </figure>
           </Link>
-          <div className={"book__title"}>
+          <div className="book__title">
             <Link to={`/books/${book.id}`} className="book__title--link">
               {book.title}
             </Link>
@@ -48,14 +44,14 @@ const mountedRef = useRef (true)
         </>
       ) : (
         <>
-          <div className="book__img--skeleton">
-            <div className="skeleton book__title--skeleton"></div>
-            <div className="skeleton book__rating--skeleton"></div>
-            <div className="skeleton book__price--skeleton"></div>
-          </div>{" "}
+          <div className="book__img--wrapper skeleton"></div>
+          <div className="skeleton book__title--skeleton"></div>
+          <div className="skeleton book__rating--skeleton"></div>
+          <div className="skeleton book__price--skeleton"></div>
         </>
       )}
     </div>
   );
 };
+
 export default Book;
